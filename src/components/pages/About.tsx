5d2149398a7156732a4bf20313abd33ce66b80dd
Bayel Erkinbekov <2001shermurzaev@gmail.com>
2025-07-25 18:23:11 +0600

    отпимизация аудио и изображения

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -109,7 +109,7 @@ const About: React.FC = () => {
             transition={{ duration: 1 }}
         >
             <div className="about-img">
-                <img src="/assets/main.jpg" alt="Bayel" loading="lazy" />
+                <img src="/assets/main.jpg" alt="Bayel" loading="eager" />
             </div>
             <div className="about-content">
                 <h1>Привет, я Байэль</h1>
